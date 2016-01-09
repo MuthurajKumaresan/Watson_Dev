1c413ceba6a11bfcc7580fa9e1f2d5c5bb647b66
MuthurajKumaresan <mzkumaresan@gmail.com>
2016-01-09 15:33:22 +0800

    personality_insight - new script

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var watson = require('watson-developer-cloud');
+var fs = require('fs');
+
+
+var personality_insights = watson.personality_insights({
+  username:'22db58af-83d2-4280-b708-5babb951fef4',
+  password:'y2lGckS8p6Yl',
+  version:'v2'
+});
+
+fs.readFile('./words.txt','utf8',function(err,data)
+{
+  if (err) throw err;
+  personality_insights.profile({ text: data },
+    function (err, response) {
+      if (err)
+      console.log('error',err);
+      else
+      {
+      var output = JSON.stringify(response,null,2);
+      console.log(output);
+      fs.writeFile('./output.txt',output);
+      }
+  });
+});
